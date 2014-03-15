5408fc42c7beecce194e5bb03e46d79849061839
Martin Gwarada <mar36tin@gmail.com>
2014-03-15 15:19:58 +0000

    Ship.scala methods added

--- src/battleship/Ship.scala
+++ src/battleship/Ship.scala
@@ -2,4 +2,28 @@ package battleship
 
 trait Ship {
 
+	abstract int getLength(){ }
+	
+	int getBowRow(){ }
+	
+	int getBowColumn(){ }
+	
+	boolean isHorizontal(int row){ }
+	
+	void setBowRow(int row){ }
+	
+	void setBowColumn(int column){ }
+	
+	void setHorizontal(boolean horizontal){ }
+	
+	abstract String getShipType(){ }
+	
+	boolean okToPlaceShipAt(int row, int column, boolean hori, Ocean ocean){ }
+	
+	void placeShipAt(int row, int column, boolean horizontal, Ocean ocean) { }
+	
+	boolean shootAt(int row, int column){ }
+	
+	boolean isSunk(){ }
+
 }

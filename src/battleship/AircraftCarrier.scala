762ab4dde35f67cf14592db2cd999fb34878e913
Don sasikumar <donsasikumar@outlook.com>
2014-04-24 18:59:57 +0000

    AircraftCarrier.scala edited online with Bitbucket

--- src/battleship/AircraftCarrier.scala
+++ src/battleship/AircraftCarrier.scala
@@ -1,5 +1,59 @@
 package battleship
 
-class AircraftCarrier {
+class AircraftCarrier(
+      length:Int = 5
+  ) extends Ship{
 
+	override def getLength:Int={
+	  length
+	}
+	
+	
+	override def getBowRow{
+	  
+	}
+	
+	override def getBowColumn{
+	  
+	}
+	
+	override def isHorizontal{
+	  
+	}
+	
+	override def setBowRow(row: Int){
+	  
+	}
+	
+	override def setBowColumn(column: Int){
+	  
+	}
+	
+	override def setHorizontal(horizontal: Boolean){ }
+	
+	override def getShipType()={
+	  "AircraftCarrier"
+	}
+	
+	override def okToPlaceShipAt(row: Int, column: Int, hori: Boolean, ocean: Object): Boolean={
+	  false
+	  
+	}
+	
+	override def placeShipAt(row: Int, column: Int, horizontal: Boolean, ocean: Object) {
+	  
+	}
+	
+	override def shootAt(row: Int, column: Int): Boolean={ 
+	  true
+	}
+	
+	override def isSunk(): Boolean={ 
+	  false
+	}
+	
+	override def toString():String={
+		if(isSunk) "x" else "S"  
+	}
 }
+  

4a0e48ebf5c20414644153ab393df9284121aa4c
Martin Gwarada <mar36tin@gmail.com>
2014-04-26 15:47:36 +0000

    PatrolBoat.scala edited online with Bitbucket

--- src/battleship/PatrolBoat.scala
+++ src/battleship/PatrolBoat.scala
@@ -1,56 +1,15 @@
 package battleship
 
-class PatrolBoat(
-      length:Int = 1
-  ) extends Ship{
+class PatrolBoat( bowR:Int, bowCol:Int, hori:Boolean) extends Ship{
+	
+	var bowRow:Int = bowR
+	var bowColumn:Int=bowCol
+	var horizontal:Boolean=hori
+	var length:Int=1
 
-	override def getLength:Int={
-	  length
-	}
 	
+	override def getShipType() = "PatrolBoat"
 	
-	override def getBowRow{
-	  
-	}
-	
-	override def getBowColumn{
-	  
-	}
-	
-	override def isHorizontal{
-	  
-	}
-	
-	override def setBowRow(row: Int){
-	  
-	}
-	
-	override def setBowColumn(column: Int){
-	  
-	}
-	
-	override def setHorizontal(horizontal: Boolean){ }
-	
-	override def getShipType()={
-	  "PatrolBoat"
-	}
-	
-	override def okToPlaceShipAt(row: Int, column: Int, hori: Boolean, ocean: Object): Boolean={
-	  false
-	  
-	}
-	
-	override def placeShipAt(row: Int, column: Int, horizontal: Boolean, ocean: Object) {
-	  
-	}
-	
-	override def shootAt(row: Int, column: Int): Boolean={ 
-	  true
-	}
-	
-	
-	override def toString():String={
-		if(isSunk) "x" else "S"  
-	}	
 }
+  
   
